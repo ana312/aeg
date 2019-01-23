18904b89a170326c2f1e7e01d8d255b877189b12
ana312 <ana312@ic.ac.uk>
2019-01-23 15:42:04 +0000

    Removed ticker

--- vanilla.py
+++ vanilla.py
@@ -1,19 +1,10 @@
 from pandas_datareader import data as pdr 
 from sp500 import SP500
+from ticker import SP_data
 import fix_yahoo_finance as yf 
 import pandas as pd
 import numpy as np
 import datetime
-
-def SP_data():
-	yf.pdr_override()
-
-	end_date = datetime.date.today() 
-	start_date = end_date + datetime.timedelta(-30)
-
-	data=pdr.get_data_yahoo(SP500(), start=start_date.isoformat(), end=end_date.isoformat())
-	return data
-	#data.to_csv('data.csv', index=False)
 
 def exp_return(data):
 	operable_data=data.Open.values
