82dcf1bd95545ce1f96b8e4773fef57d91b78ffc
ana312 <ana312@ic.ac.uk>
2019-01-23 12:13:05 +0000

    Ticker V1

--- ticker.py
+++ ticker.py
@@ -1,5 +1,17 @@
 from pandas_datareader import data as pdr 
+from sp500 import SP500
 import fix_yahoo_finance as yf 
+import pandas as pd
+import numpy as np
+import datetime
 
-yf.pdr_override() 
-data=pdr.get_data_yahoo("SPY", start="2018-01-01", end="2018-04-30")
+yf.pdr_override()
+
+end_date = datetime.date.today() 
+
+start_date = end_date + datetime.timedelta(-30)
+
+
+data=pdr.get_data_yahoo(SP500(), start=start_date.isoformat(), end=end_date.isoformat())
+data.to_csv('data.csv', index=False)
+
