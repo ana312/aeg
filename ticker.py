3f8646b83a0461896eadfb2b97d0aea20140bf89
ana312 <ana312@ic.ac.uk>
2019-01-27 15:23:19 +0000

    Ticker Comments

--- ticker.py
+++ ticker.py
@@ -1,3 +1,6 @@
+#Returns SP500 Data from Yahoo over j-day interval from today
+#Takes j, no. of days data to return 
+
 from pandas_datareader import data as pdr 
 from sp500 import SP500
 import fix_yahoo_finance as yf 
@@ -5,11 +8,12 @@ import pandas as pd
 import numpy as np
 import datetime
 
-def SP_data():
+
+def SP_data(j):
 	yf.pdr_override()
 
 	end_date = datetime.date.today() 
-	start_date = end_date + datetime.timedelta(-30)
+	start_date = end_date + datetime.timedelta(-j)
 
 	data=pdr.get_data_yahoo(SP500(), start=start_date.isoformat(), end=end_date.isoformat())
 	return data
